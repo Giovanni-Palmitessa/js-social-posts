5b2e297cc84b1a700e5a759eaf815b36b8b04b8f
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-03 17:23:36 +0200

    add hover for like button

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -92,10 +92,10 @@ for (let i = 0; i < posts.length; i++) {
                 <div class="post__footer">
                     <div class="likes js-likes">
                         <div class="likes__cta">
-                            <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                            <div class="like-button  js-like-button" data-postid="${post.id}">
                                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                                 <span class="like-button__label">Mi Piace</span>
-                            </a>
+                            </div>
                         </div>
                         <div class="likes__counter">
                             Piace a <b id="like-counter-1" class="js-likes-counter">${likeNumber}</b> persone
